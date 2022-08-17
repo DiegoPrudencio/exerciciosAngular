7f86f7bb03c77a731126a9d1bde01ba94959a212
Diego <diegoprudencioo@gmail.com>
2022-08-17 08:16:22 -0300

    add new component

---
+++ src/app/autenticacao/autenticacao.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-autenticacao',
+    templateUrl: './app.autenticacao.html',
+    styleUrls: ['./app.autenticacao.css']
+})
+
+export class AutenticacaoComponent {
+
+
+}
