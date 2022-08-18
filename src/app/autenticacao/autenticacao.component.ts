dabee03cf24662b6bb81c890cd5a97393f502dc9
Diego <diegoprudencioo@gmail.com>
2022-08-18 19:10:44 -0300

    add exercícios Lista2

--- src/app/autenticacao/autenticacao.component.ts
+++ src/app/autenticacao/autenticacao.component.ts
@@ -8,5 +8,43 @@ import { Component } from "@angular/core";
 
 export class AutenticacaoComponent {
 
+    email: string = "";
+    password: string = "";
+    message: string = "";
+    contador: number = 0;
+    color: string = "";
+
+
+    public login() {
+
+
+        this.contador++
+
+
+            if (this.contador < 3) {
+                if ((this.email == "XPTO-21") && (this.password == "Trocar@123")) {
+                    this.message = "Logado!"
+                    this.color = "green"
+                } else if ((this.email != "XPTO-21") && (this.password == "Trocar@123")) {
+                    this.message = "Acesso negado, usuário incorreto";
+                    this.color = "red"
+                    this.email = "";
+                    this.password = "";                    
+                } else if ((this.email == "XPTO-21") && (this.password != "Trocar@123")) {
+                    this.message = "Acesso negado, senha incorreta"
+                    this.color = "red"
+                    this.email = "";
+                    this.password = "";
+                }
+                
+            } else if (this.contador === 3) {
+                    this.message = "Usuário Bloqueado!"
+            
+            }
+                   
+            console.log(this.contador);
+
+
+    }
 
 }
